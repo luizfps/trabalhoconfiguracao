d97f635d19f3fa1cbbc069ee27de1e26691f60d7
luiz felipe <luizfelipe_p@hotmail.com>
2016-02-03 12:47:22 -0200

    classe A criada

---
+++ A.java
@@ -0,0 +1,23 @@
+public class classA {
+
+	private int A1;
+	private float A2;
+	
+	
+	public void setA1(string a3) {
+		A3 = a3;
+	}
+	
+	public int getA1() {
+		return A1;
+	}
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+	public float getA2() {
+		return A2;
+	}
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+}
