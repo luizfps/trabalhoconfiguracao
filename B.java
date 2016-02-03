09b90e2424f4f9c799536f298ccf533978cfcb1b
luiz felipe <luizfelipe_p@hotmail.com>
2016-02-03 14:50:03 -0200

    classe b criada

---
+++ B.java
@@ -0,0 +1,18 @@
+public class classB {
+	private int B1;
+	private float B2;
+	
+	public int getB1() {
+		return B1;
+	}
+	public void setB1(int b1) {
+		B1 = B1;
+	}
+	public float getB2() {
+		return B2;
+	}
+	public void setB2(float B2) {
+		B2 = B2;
+	}
+		
+}
